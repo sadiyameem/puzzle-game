01293fae09fc7b9f6ee6e1a2b9fe716ab8481744
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-12-22 14:10:53 -0500

    adding js to make rows and picture appear

---
+++ script.js
@@ -0,0 +1,16 @@
+var rows = 5;
+var colums = 5;
+
+var currTile;
+var otherTile;
+
+var turns = 0; // keeps track of the number of turns
+
+window.onload = function() {
+    // initialize the 5x5 game board
+    for (let r = 0; r < rows; r++) {
+        // img tag
+        let tile = document.createElement("img");
+        tile.src
+    }
+}
