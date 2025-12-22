67a5bd602fd0a5283f160dbc6c200f62b48181ba
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-12-22 17:23:34 -0500

    rewriting all pf my code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var rows = 5;
-var colums = 5;
+var columns = 5;
 
 var currTile;
 var otherTile;
@@ -9,8 +9,24 @@ var turns = 0; // keeps track of the number of turns
 window.onload = function() {
     // initialize the 5x5 game board
     for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
         // img tag
+            let tile = document.createElement("img");
+            tile.src = ".avocado.jpg";
+
+            document.getElementById("board").append(tile);
+        }
+    }
+
+    //pieces
+    let pieces = [];
+    for (let i=1; i <= rows*columns; i ++) {
+        pieces.push(i.toString()); // put 1 to 25 into the array (puzzle image names)
+    }
+
+    for (let i =0; i < pieces.length; i++) {
         let tile = document.createElement("img");
-        tile.src
+        tile.src = "avocado" + pieces[i] + ".jpg";
+        document.getElementById("pieces").append(tile);
     }
 }
