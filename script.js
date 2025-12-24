53312ceee1b3f45c78e570a7503cd345260fc3c6
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2025-12-24 17:14:39 -0500

    fixed errors and changed colors

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     const isAdjacent = (index1,index2)=>{
         const [row1,col1] = [Math.floor(index1 / 3), index1 % 3];
-        const [row2,col2] = [Math.floor(index1 / 3), index1 % 3];
+        const [row2,col2] = [Math.floor(index2 / 3), index1 % 3];
         return (Math.abs(row1 - row2) + Math.abs(col1 - col2)) === 1;
     }
 
